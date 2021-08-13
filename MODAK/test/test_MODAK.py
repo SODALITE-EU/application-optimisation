f77c2099b6ee7a20ff28f3d66c92d0334fa5db8c
Tiziano Müller <tiziano.mueller@hpe.com>
2021-08-13 14:28:00 +0200

    drop duplicated scripts, reorganize Dockerfile and docker-compose, start fixing tests
    drop wget package, fixes #27

--- MODAK/test/test_MODAK.py
+++ MODAK/test/test_MODAK.py
@@ -1,9 +1,6 @@
 import json
 import pathlib
-import ssl
 import unittest
-
-import wget
 
 from MODAK import MODAK
 
@@ -20,14 +17,13 @@ class test_MODAK(unittest.TestCase):
     def test_modak_hpc(self):
         print("Test MODAK")
         m = MODAK()
-        ssl._create_default_https_context = ssl._create_unverified_context
         dsl_file = SCRIPT_DIR / "input" / "mpi_solver.json"
         with dsl_file.open() as json_file:
             job_link = m.optimise(json.load(json_file))
-        filename = wget.download(job_link, out="/tmp")
+
         with SCRIPT_DIR.joinpath("input/solver.sh").open() as fhandle:
             mylist = list(fhandle)
-        with open(filename) as fhandle:
+        with open(job_link) as fhandle:
             testlist = list(fhandle)
         self.assertEqual(len(mylist), len(testlist))
 
@@ -43,11 +39,11 @@ class test_MODAK(unittest.TestCase):
         with dsl_file.open() as json_file:
             job_link = m.optimise(json.load(json_file))
 
-        ssl._create_default_https_context = ssl._create_unverified_context
-        filename = wget.download(job_link, out="/tmp")
-        with SCRIPT_DIR.joinpath("input/skyline-extraction-training.sh") as fhandle:
+        with SCRIPT_DIR.joinpath(
+            "input/skyline-extraction-training.sh"
+        ).open() as fhandle:
             mylist = list(fhandle)
-        with open(filename) as fhandle:
+        with open(job_link) as fhandle:
             testlist = list(fhandle)
         self.assertEqual(len(mylist), len(testlist))
 
@@ -62,11 +58,9 @@ class test_MODAK(unittest.TestCase):
         with dsl_file.open() as json_file:
             job_link = m.optimise(json.load(json_file))
 
-        ssl._create_default_https_context = ssl._create_unverified_context
-        filename = wget.download(job_link, out="/tmp")
-        with SCRIPT_DIR.joinpath("input/resnet.sh") as fhandle:
+        with SCRIPT_DIR.joinpath("input/resnet.sh").open() as fhandle:
             mylist = list(fhandle)
-        with open(filename) as fhandle:
+        with open(job_link) as fhandle:
             testlist = list(fhandle)
         self.assertEqual(len(mylist), len(testlist))
 
@@ -81,11 +75,9 @@ class test_MODAK(unittest.TestCase):
         with dsl_file.open() as json_file:
             job_link = m.optimise(json.load(json_file))
 
-        ssl._create_default_https_context = ssl._create_unverified_context
-        filename = wget.download(job_link, out="/tmp")
-        with SCRIPT_DIR.joinpath("input/mpi_test.sh") as fhandle:
+        with SCRIPT_DIR.joinpath("input/mpi_test.sh").open() as fhandle:
             mylist = list(fhandle)
-        with open(filename) as fhandle:
+        with open(job_link) as fhandle:
             testlist = list(fhandle)
         self.assertEqual(len(mylist), len(testlist))
 
@@ -100,12 +92,11 @@ class test_MODAK(unittest.TestCase):
         with dsl_file.open() as json_file:
             job_link = m.optimise(json.load(json_file))
 
-        ssl._create_default_https_context = ssl._create_unverified_context
-        filename = wget.download(job_link, out="/tmp")
-        with SCRIPT_DIR.joinpath("input/mpi_test_egi.sh") as fhandle:
+        with SCRIPT_DIR.joinpath("input/mpi_test_egi.sh").open() as fhandle:
             mylist = list(fhandle)
-        with open(filename) as fhandle:
+        with open(job_link) as fhandle:
             testlist = list(fhandle)
+
         self.assertEqual(len(mylist), len(testlist))
 
         for i in range(0, (len(mylist))):
