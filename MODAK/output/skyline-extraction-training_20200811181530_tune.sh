6d590ccffe55049e58b7a4c61a9b939668850da2
Tiziano Müller <tiziano.mueller@hpe.com>
2021-09-03 10:13:19 +0200

    output: drop generated files from git, configure gitignore

--- MODAK/output/skyline-extraction-training_20200811181530_tune.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## START OF TUNER ##
-wget --no-check-certificate https://www.dropbox.com/s/iaivd8wujhctpl5/cresta.tar.gz
-tar -xvf cresta.tar.gz
-export PATH=$PATH:$(pwd)/cresta
-wget --no-check-certificate https://storage.googleapis.com/modak-bucket//modak/skyline-extraction-training_20200811181530.tune
-tune skyline-extraction-training_20200811181530.tune
-## END OF TUNER ##
