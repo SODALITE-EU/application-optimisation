34e63a66fc23d2cca17c3f641e50e76c94538c4a
Tiziano Müller <tiziano.mueller@hpe.com>
2021-09-03 10:13:19 +0200

    output: drop generated files from git, configure gitignore

--- MODAK/output/solver_20200811181533_tune.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## START OF TUNER ##
-wget --no-check-certificate https://www.dropbox.com/s/iaivd8wujhctpl5/cresta.tar.gz
-tar -xvf cresta.tar.gz
-export PATH=$PATH:$(pwd)/cresta
-wget --no-check-certificate https://storage.googleapis.com/modak-bucket//modak/solver_20200811181533.tune
-tune solver_20200811181533.tune
-## END OF TUNER ##
