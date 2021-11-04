a7773088372d087654e36ba653d6e10de183c4ff
Tiziano Müller <tiziano.mueller@hpe.com>
2021-11-04 14:42:11 +0100

    drop the scripts since they are now managed directly inside the DB

--- MODAK/MODAK/scripts/enable_xla.sh
+++
@@ -1,3 +0,0 @@
-mkdir xla_dump
-export TF_XLA_FLAGS="--tf_xla_auto_jit=2 --tf_xla_cpu_global_jit"
-export XLA_FLAGS="--xla_dump_to=xla_dump/generated"
--- MODAK/MODAK/scripts/set_default_cirrus.sh
+++
@@ -1,6 +0,0 @@
-## MODAK: START CIRRUS  ##
-module load mpt
-module load intel-compilers-18
-module load singularity
-## MODAK: END CIRRUS ##
-
