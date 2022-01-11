b0ec13ce9fc211b2a94fde655c0869b76953db41
Tiziano Müller <tiziano.mueller@hpe.com>
2022-01-11 16:48:54 +0100

    setup.py: set version to 1.1.0

--- MODAK/setup.py
+++ MODAK/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="MODAK",
-    version="0.1.0",
+    version="1.1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
